7867c046643ee5faf689d583cc88fe4f9f6ff07f
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-02-26 23:17:10 +0900

    입력시 목록에 내용 추가 기능 만듬

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const todoList = document.getElementById("todo-list");
+const todoInput = document.getElementById("todo-input");
 
 const keyCodeCheck = function () {
-    if (window.event.keyCode === 13) {
-        const inputValue = document.getElementById("todo-input").value;
+    if (window.event.keyCode === 13 && todoInput.value !== "") {
+        const todoList = document.getElementById("todo-list");
         const newLi = document.createElement("li");
         const newSpan = document.createElement("span");
 
-        newSpan.textContent = inputValue;
+        newSpan.textContent = todoInput.value;
         newLi.appendChild(newSpan);
         todoList.appendChild(newLi);
+        todoInput.value = "";
     }
 };
