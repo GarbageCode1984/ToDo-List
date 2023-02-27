d79bcf4775a4b3180b55ea31f108e152984478bc
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-02-27 23:35:17 +0900

    newBtn 추가

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const todoInput = document.getElementById("todo-input");
 
+const createTodo = function () {
+    const todoList = document.getElementById("todo-list");
+    const newLi = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newBtn = document.createElement("button");
+
+    newBtn.addEventListener("click", () => {
+        newLi.classList.toggle("complete");
+    });
+
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    todoInput.value = "";
+};
+
 const keyCodeCheck = function () {
     if (window.event.keyCode === 13 && todoInput.value !== "") {
-        const todoList = document.getElementById("todo-list");
-        const newLi = document.createElement("li");
-        const newSpan = document.createElement("span");
-
-        newSpan.textContent = todoInput.value;
-        newLi.appendChild(newSpan);
-        todoList.appendChild(newLi);
-        todoInput.value = "";
+        createTodo();
     }
 };
