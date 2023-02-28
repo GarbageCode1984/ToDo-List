94a0a12320a5cc00ec3173c8f16ab6d82fa8c97c
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-02-28 22:03:43 +0900

    saveItemsFn 생성

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
 
 const createTodo = function () {
-    const todoList = document.getElementById("todo-list");
     const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
     const newBtn = document.createElement("button");
@@ -10,15 +10,39 @@ const createTodo = function () {
         newLi.classList.toggle("complete");
     });
 
+    newLi.addEventListener("dblclick", () => {
+        newLi.remove();
+    });
+
     newSpan.textContent = todoInput.value;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
     todoInput.value = "";
+    saveItemsFn();
 };
 
 const keyCodeCheck = function () {
     if (window.event.keyCode === 13 && todoInput.value !== "") {
         createTodo();
     }
+};
+
+const deleteAll = function () {
+    const liList = document.querySelectorAll("li");
+    for (let i = 0; i < liList.length; i++) {
+        liList[i].remove();
+    }
+};
+
+const saveItemsFn = function () {
+    const saveItems = [];
+    for (let i = 0; i < todoList.children.length; i++) {
+        const todoObj = {
+            contents: todoList.children[i].querySelector("span").textContent,
+            complete: todoList.children[i].classList.contains("complete"),
+        };
+        saveItems.push(todoObj);
+    }
+    console.log(saveItems);
 };
