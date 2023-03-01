c6bddfc6640a7c20f51fb2e746aece1e77f84c7b
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-03-02 01:46:31 +0900

    TodoList 기능 구현 끝

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 
-const createTodo = function () {
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = function (storageData) {
+    let todoContents = todoInput.value;
+
+    if (storageData) {
+        todoContents = storageData.contents;
+    }
+
     const newLi = document.createElement("li");
     const newSpan = document.createElement("span");
     const newBtn = document.createElement("button");
 
     newBtn.addEventListener("click", () => {
         newLi.classList.toggle("complete");
+        saveItemsFn();
     });
 
     newLi.addEventListener("dblclick", () => {
         newLi.remove();
+        saveItemsFn();
     });
 
-    newSpan.textContent = todoInput.value;
+    if (storageData?.complete) {
+        newLi.classList.add("complete");
+    }
+
+    newSpan.textContent = todoContents;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
@@ -33,6 +47,7 @@ const deleteAll = function () {
     for (let i = 0; i < liList.length; i++) {
         liList[i].remove();
     }
+    saveItemsFn();
 };
 
 const saveItemsFn = function () {
@@ -44,5 +59,13 @@ const saveItemsFn = function () {
         };
         saveItems.push(todoObj);
     }
-    console.log(saveItems);
+    saveItems.length === 0
+        ? localStorage.removeItem("saved-items")
+        : localStorage.setItem("saved-items", JSON.stringify(saveItems));
 };
+
+if (savedTodoList) {
+    for (let i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
+    }
+}
